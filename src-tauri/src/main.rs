c610492782dd181a6b998002a9b01d5bffecc8d1
d1msk1y <yatcenko.dmitriy@gmail.com>
2023-03-24 11:23:52 +0100

    Receiving messages json from the server via get_messages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use reqwest::Error;
+use tauri::CursorIcon::Text;
+
+async fn get_request() -> Result<String, Error>{
+    let response = reqwest::get("http://localhost:8080/messages")
+        .await?
+        .text()
+        .await?;
+    println!("{}", response);
+    Ok(format!("{}", response))
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_messages() -> Result<String, String>{
+    get_request().await.map_err(|e| e.to_string())
 }
 
 fn main() {
+    get_messages();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_messages
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
