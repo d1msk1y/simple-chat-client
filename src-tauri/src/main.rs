630f559e9d05a57568d9eba391a81439a6354243
d1msk1y <yatcenko.dmitriy@gmail.com>
2023-03-27 18:11:52 +0200

    get_all_messages and get_last_message endpoints are implemented in both backend and frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -21,14 +21,28 @@ async fn get_request(endpoint: &str) -> Result<String, Error>{
 #[tauri::command]
 async fn get_message_by_id(id: &str) -> Result<String, String>
 {
-    let url = "/messages/".to_owned() + id;
-    get_request(url.as_str()).await.map_err(|e| e.to_string())
+    let endpoint = "/messages/".to_owned() + id;
+    get_request(endpoint.as_str()).await.map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn get_all_messages() -> Result<String, String> {
+    let endpoint = "/messages";
+    get_request(endpoint).await.map_err(|e|e.to_string())
+}
+
+#[tauri::command]
+async fn get_last_message() -> Result<String, String> {
+    let endpoint = "/messages/last";
+    get_request(endpoint).await.map_err(|e|e.to_string())
 }
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            get_message_by_id
+            get_message_by_id,
+            get_all_messages,
+            get_last_message
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ const { invoke } = window.__TAURI__.tauri;
 
 let greetMsgEl;
 
-async function getMessages() {
-  greetMsgEl.textContent = await invoke("get_message_by_id", {id: "1"});
+async function getMessageById() {
+  greetMsgEl.textContent = await invoke("get_message_by_id", {id: "0"});
+}
+
+async function getLastMessage() {
+  greetMsgEl.textContent = await invoke("get_last_message");
+}
+
+async function getAllMessages() {
+  greetMsgEl.textContent = await invoke("get_all_messages");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
