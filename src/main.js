4167b05c8e53eda38b838b02a8d483d2c50c59da
d1msk1y <yatcenko.dmitriy@gmail.com>
2023-03-28 15:33:51 +0200

    Printing parsed json object as a message box

--- src/main.js
+++ src/main.js
@@ -10,13 +10,26 @@ function createMessageGetter(methodName, params = {}) {
   }
 }
 
+function createMessageBox(message, nickname){
+  const timestamp = new Date().toLocaleTimeString();
+  const messageBoxHTML = `
+      <div style="background-color: #f2f2f2; border-radius: 10px; padding: 10px; max-width: 300px;">
+        <p style="font-size: 14px; margin: 0;">${message}</p>
+        <p style="font-size: 12px; margin: 0; color: #8c8c8c;">Sent at ${timestamp}</p>
+      </div>
+    `;
+  return document.createRange().createContextualFragment(messageBoxHTML).firstElementChild;
+}
+
+
 const getMessageById = createMessageGetter("get_message_by_id", {id: "0"});
 const getLastMessage = createMessageGetter("get_last_message");
 const getAllMessages = createMessageGetter("get_all_messages");
 
 async function printLastMessage() {
   const message = await getLastMessage();
-  greetMsgEl.textContent = message.message;
+  const messageBox = createMessageBox(message.message, message.nickname)
+  document.body.appendChild(messageBox);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
